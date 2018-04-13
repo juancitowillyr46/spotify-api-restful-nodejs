0a92f390d92c523eb6007d815b682b035a580277
Juan Rodas <juan.rodas.manez@gmail.com>
2018-04-13 12:17:16 -0500

    Album Upload Image, get Image

--- controllers/album.js
+++ controllers/album.js
@@ -94,9 +94,80 @@ function updateAlbum(req, res){
 
 }
 
+function deleteAlbum(req, res){
+
+    var albumId = req.params.id;
+    
+    Album.findByIdAndRemove(albumId).remove((err, albumRemoved) => {
+        if(err){
+            res.status(500).send({message: 'Error al eliminar el album'});
+        } else {
+            if(!albumRemoved){
+                res.status(404).send({message: 'El album no ha sido eliminado'});
+            }else{
+                Song.find({album: albumRemoved._id}).remove((err, songRemoved) => {
+                    if(err){
+                        res.status(500).send({message: 'Error al eliminar la canción'});
+                    }else{
+                        if(!songRemoved){
+                            res.status(404).send({message: 'La canción no ha sido eliminada'});
+                        }else{
+                            res.status(200).send({album: albumRemoved});
+                        }
+                    }
+                })
+            }
+        }
+    });
+
+}
+
+function uploadImage(req, res){
+    var albumId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+            Album.findByIdAndUpdate(albumId, {image: file_name}, (err, albumUpdated) => {
+                if(!albumUpdated){
+                    res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+                }else{
+                    res.status(200).send({album: albumUpdated});
+                }
+            });    
+        }else{
+            res.status(200).send({message: 'Extensión del archivo no valida'});
+        }
+    }else{
+        res.status(200).send({message: 'No has subido ninguna imagen...'});
+    }
+}
+
+function getImageFile(req, res){
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/albums/'+imageFile;
+    fs.exists(path_file, function(exists){
+        if(exists){ 
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'No existe la imagen'});
+        }
+    });
+}
+
 module.exports = {
     getAlbum,
     saveAlbum,
     getAlbums,
-    updateAlbum
+    updateAlbum,
+    deleteAlbum,
+    uploadImage,
+    getImageFile
 }
